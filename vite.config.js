8e26834aa184bc5f8963516b7816f4e992afbe03
2min0623 <blackmaple.hsiao@gmail.com>
2024-08-26 17:04:28 +0800

    0826_1704

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import {fileURLToPath, URL} from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tcivs_fablab/',
   plugins: [vue()],
   resolve: {
     alias: {
